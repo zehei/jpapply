8b2c0998d3c90632a5cddad6fd22c8f8e75541d4
Ze Hei <zehei@Zes-MacBook-Pro.local>
2019-11-27 18:54:09 +0800

    react page added

---
+++ precache-manifest.b0630d26e15f0854dda79bccc42f560b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb60af77b36aab20b7fb96badd6c6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13269c872f5481ce9f7a",
+    "url": "/static/css/main.42833b72.chunk.css"
+  },
+  {
+    "revision": "913cf35706aa7fb497f2",
+    "url": "/static/js/2.2ee53afe.chunk.js"
+  },
+  {
+    "revision": "13269c872f5481ce9f7a",
+    "url": "/static/js/main.9467b795.chunk.js"
+  },
+  {
+    "revision": "81582909e04bb7e19f1c",
+    "url": "/static/js/runtime-main.5688d99c.js"
+  }
+]);
